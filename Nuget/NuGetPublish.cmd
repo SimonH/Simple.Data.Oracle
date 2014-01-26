aa2a1088ff2f0940221f88aa2a06b1d32286c0db
halex2005 <halex2005@e1.ru>
2014-01-27 01:39:48 +0600

    added nuspec files for Devart and ManagedDataAccess. updated scripts for copy/pack/publish release

--- Nuget/NuGetPublish.cmd
+++ Nuget/NuGetPublish.cmd
@@ -1,2 +1,4 @@
-nuget push Simple.Data.Oracle.%1.nupkg
+nuget push "%~dp0Simple.Data.Oracle.%1.nupkg"
+nuget push "%~dp0Simple.Data.Oracle.Devart.%1.nupkg"
+nuget push "%~dp0Simple.Data.Oracle.ManagedDataAccess.%1.nupkg"
 pause
