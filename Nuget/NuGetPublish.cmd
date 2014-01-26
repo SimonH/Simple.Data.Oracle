a10bb54c267376c21c2571ea57f3583cf5c20633
halex2005 <halex2005@e1.ru>
2014-01-27 01:51:31 +0600

    updated nuget publish script for specify correct nuget.exe location and paths dealing with spaces

--- Nuget/NuGetPublish.cmd
+++ Nuget/NuGetPublish.cmd
@@ -1,4 +1,4 @@
-nuget push "%~dp0Simple.Data.Oracle.%1.nupkg"
-nuget push "%~dp0Simple.Data.Oracle.Devart.%1.nupkg"
-nuget push "%~dp0Simple.Data.Oracle.ManagedDataAccess.%1.nupkg"
+"%~dp0..\.nuget\nuget.exe" push "%~dp0Simple.Data.Oracle.%1.nupkg"
+"%~dp0..\.nuget\nuget.exe" push "%~dp0Simple.Data.Oracle.Devart.%1.nupkg"
+"%~dp0..\.nuget\nuget.exe" push "%~dp0Simple.Data.Oracle.ManagedDataAccess.%1.nupkg"
 pause
